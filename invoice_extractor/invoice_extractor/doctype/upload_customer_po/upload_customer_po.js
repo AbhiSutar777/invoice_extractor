58511594cf1858a868336fef4639ca2ded464faa
abhijeet <abhijeet@ubuntu.myguest.virtualbox.org>
2024-10-17 22:49:07 +0530

    ADD: New doctype 'Upload Customer PO'

---
+++ invoice_extractor/invoice_extractor/doctype/upload_customer_po/upload_customer_po.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Abhijeet Sutar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Upload Customer PO", {
+// 	refresh(frm) {
+
+// 	},
+// });
